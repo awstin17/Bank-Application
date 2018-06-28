24a0ce400f5e5c8b953e0d52aa0743a30f3a72c6
Peter Horton <phorton@softstackfactory.org>
2018-06-28 05:30:50 +0000

    Got routing for atm and teller views to work

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,10 +1,14 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AtmComponent } from './atm/atm.component';
+import {TellerComponent } from './teller/teller.component';
 
 export const router: Routes = [
   { path: '', redirectTo: 'atm', pathMatch: 'full' },
-  { path: 'app', component: AppComponent }
+  { path: 'app', component: AppComponent },
+  {path: 'atm', component: AtmComponent },
+  {path: 'teller', component: TellerComponent }
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
